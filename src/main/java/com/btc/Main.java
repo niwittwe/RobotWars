24a63a13b1218a4a8118331bf9c5494a8f92656e
Nils Daniel Wittwer <nilsdaniel.wittwer@btc-ag.com>
2024-09-18 15:10:59 +0200

    Spieler wird mit einer ASCII Art Grafik begrüßt

--- src/main/java/com/btc/Main.java
+++ src/main/java/com/btc/Main.java
@@ -2,6 +2,15 @@ package com.btc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("       __");
+        System.out.println("  ___[LL]_______________________");
+        System.out.println(" /                                 \\");
+        System.out.println("|    _   _   _   _   _   _|      \\");
+        System.out.println("|  | | | | | | | | | | | | |  __  |");
+        System.out.println("|  |_| |_| |_| |_| |_| |_| |_|| |_|");
+        System.out.println("|_________________________________|");
+        System.out.println(" |_______________________________|");
+        System.out.println();
+        System.out.println("   // War Bot 1           War Bot 2 \\\\");
     }
 }
